69d80ad82e21a7e1025befa9c4fda7c43d332d5a
Jeger0 <563keyboard@gmail.com>
2026-02-24 14:20:50 +0100

    feat: setup basic folder structure and react-routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Events from './pages/Events';
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1>Ditio</h1>
-    </>
-  )
-}
+    <Router>
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events/" element={<Events />} />
+        <Route path="/about" element={<div><h1>Om Ditio</h1></div>} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/pages/Events.tsx
@@ -0,0 +1,9 @@
+export default function Events() {
+  return (
+    <div>
+      <h1>Arrangementer</h1>
+      <p>Abc</p>
+    
+    </div>
+  );
+}

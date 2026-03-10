709f6306a414f646d9044f46bbc57db19825ee98
Jeger0 <563keyboard@gmail.com>
2026-03-10 17:36:48 +0100

    Revert "Feat: navbar design"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Events from './pages/Events';
-import EventDetail from './pages/EventDetail';
-import About from './pages/About';
+import './App.css'
 
-export default function App() {
+function App() {
+
   return (
-    <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events/" element={<Events />} />
-        <Route path="/events/:id" element={<EventDetail />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <h1>Ditio</h1>
+    </>
+  )
 }
+
+export default App
--- src/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <div>
-      <h1>Om Ditio</h1>
-      <p>Abc</p>
-    
-    </div>
-  );
-}
